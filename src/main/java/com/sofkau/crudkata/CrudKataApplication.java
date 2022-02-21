f952b0913a9ea9b9f1711a37949ec8f10c84c993
Alex <alexandre_2420@outlook.com>
2022-02-21 08:38:31 -0500

    add: Todo Entity

---
+++ src/main/java/com/sofkau/crudkata/CrudKataApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.crudkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkau/crudkata/CrudKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.crudkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
